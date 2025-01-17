5f2bde1c8ae8faba4f79779547de5b21c7a5025e
DaLan <valkyrie777is@gmail.com>
2025-01-17 15:04:26 +0800

    修改12

--- src/components/Member/Company.vue
+++ src/components/Member/Company.vue
@@ -124,7 +124,7 @@ const deleteUser = () => {
       <div class="card-body">
         <table
           class="table table-bordered company-table"
-          v-if="windowWidth > 790"
+          v-if="windowWidth > 800"
         >
           <thead>
             <tr>
