2475895e7aa4e00bacf6f946bbc673d383a91b61
DaLan <valkyrie777is@gmail.com>
2025-01-20 09:25:20 +0800

    修改20

--- src/components/Member/Member.vue
+++ src/components/Member/Member.vue
@@ -135,8 +135,8 @@ onBeforeUnmount(() => {
               <th width="10%" class="text-center">使用者名稱</th>
               <th width="13%" class="text-center">使用者帳號</th>
               <th width="15%" class="text-center">電子郵件</th>
-              <th width="13%" class="text-center">電話</th>
-              <th width="7%" class="text-center">職位</th>
+              <th width="11%" class="text-center">電話</th>
+              <th width="9%" class="text-center">職位</th>
               <th width="10%" class="text-center">建立時間</th>
               <th width="10%" class="text-center">更新時間</th>
               <th width="10%" class="text-center">修改</th>
