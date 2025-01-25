b3fc6a44b4d6e5c1b80db32a1a411a6e93ccad12
DaLan <valkyrie777is@gmail.com>
2025-01-25 09:08:27 +0800

    修改73

--- src/components/Product/ProductCreate.vue
+++ src/components/Product/ProductCreate.vue
@@ -328,7 +328,9 @@ onBeforeUnmount(() => {
           </div>
           <div class="mt-4">
             <label for="member_email">產品標籤</label>
-            <div class="my-2 d-flex align-items-center product-classify-row-style">
+            <div
+              class="my-2 d-flex align-items-center product-classify-row-style"
+            >
               <div
                 class="form-check form-check-inline product-classify-style"
                 v-for="(item, index) in productLabels"
@@ -345,7 +347,7 @@ onBeforeUnmount(() => {
                 />
                 <label
                   :for="item.itemId"
-                  class="form-check-label product-classify-name"
+                  class="form-check-label product-label-name"
                   >{{ item.itemName }}</label
                 >
               </div>
@@ -379,9 +381,11 @@ onBeforeUnmount(() => {
                   v-model="newProductData.product_sweetness"
                   @change="onInputBlur($event, 'product_sweetness')"
                 />
-                <label :for="item.itemId" class="form-check-label">{{
-                  item.itemName
-                }}</label>
+                <label
+                  :for="item.itemId"
+                  class="form-check-label product-label-name"
+                  >{{ item.itemName }}</label
+                >
               </div>
             </div>
             <ul
@@ -400,7 +404,7 @@ onBeforeUnmount(() => {
             <label for="member_email">可選擇溫度</label>
             <div class="my-2 d-flex align-items-center">
               <div
-                class="form-check form-check-inline"
+                class="form-check form-check-inline product-ice-style"
                 v-for="(item, index) in temperatureChooses"
                 :key="index"
               >
@@ -413,9 +417,11 @@ onBeforeUnmount(() => {
                   v-model="newProductData.product_ice"
                   @change="onInputBlur($event, 'product_ice')"
                 />
-                <label :for="item.itemId" class="form-check-label">{{
-                  item.itemName
-                }}</label>
+                <label
+                  :for="item.itemId"
+                  class="form-check-label product-label-name"
+                  >{{ item.itemName }}</label
+                >
               </div>
             </div>
             <ul
@@ -443,9 +449,11 @@ onBeforeUnmount(() => {
                   :value="item.itemName"
                   v-model="newProductData.product_add"
                 />
-                <label :for="item.itemId" class="form-check-label">{{
-                  item.itemName
-                }}</label>
+                <label
+                  :for="item.itemId"
+                  class="form-check-label product-label-name"
+                  >{{ item.itemName }}</label
+                >
               </div>
             </div>
           </div>
@@ -466,9 +474,11 @@ onBeforeUnmount(() => {
                   v-model="newProductData.product_address"
                   @change="onInputBlur($event, 'product_address')"
                 />
-                <label :for="item.itemId" class="form-check-label">{{
-                  item.itemName
-                }}</label>
+                <label
+                  :for="item.itemId"
+                  class="form-check-label product-label-name"
+                  >{{ item.itemName }}</label
+                >
               </div>
             </div>
             <ul
@@ -538,18 +548,18 @@ onBeforeUnmount(() => {
   opacity: 0.8;
 }
 
-.product-classify-style {
+.product-classify-style,
+.product-ice-style {
   flex: 0 0 10%;
 }
 
-.product-classify-name {
+.product-label-name {
   white-space: nowrap; /* 防止文字換行 */
 }
 
-@media (max-width: 1400px) {
+@media (max-width: 1450px) {
   .product-classify-style {
     flex: 0 0 20%; /* 每個選項占寬度增加，方便分行 */
-    margin-bottom: 100px;
   }
 
   .product-classify-row-style {
