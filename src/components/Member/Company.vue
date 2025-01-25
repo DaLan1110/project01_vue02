4c5e2f7ac91eea6cc1f58c024ee2c002077b313a
DaLan <valkyrie777is@gmail.com>
2025-01-26 06:51:17 +0800

    修改79

--- src/components/Member/Company.vue
+++ src/components/Member/Company.vue
@@ -464,7 +464,7 @@ const deleteUser = () => {
 
 .card-list {
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  /* grid-template-columns: repeat(auto-fill, minmax(300px, 1fr)); */
   gap: 20px;
   padding: 20px;
 }
