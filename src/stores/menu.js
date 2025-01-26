d57fdb2cedc8ecd17172fcecccb5454e7a0af61d
DaLan <valkyrie777is@gmail.com>
2025-01-26 10:18:36 +0800

    修改02

--- src/stores/menu.js
+++ src/stores/menu.js
@@ -1,9 +1,9 @@
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useMenuStore = defineStore('menu', () => {
     const menuFlexible = ref(false)
-    const hideAddImgProduct = ref(true)
+    const hideAddImgProduct = computed(() => !menuFlexible.value);
     const toggleMenu = () => {
         menuFlexible.value = !menuFlexible.value;
         hideAddImgProduct.value = !hideAddImgProduct.value
