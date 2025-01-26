016c87ff57fa0ec8978b392c99ddb0fa2e25936f
DaLan <valkyrie777is@gmail.com>
2025-01-26 09:38:45 +0800

    修改90

--- src/components/Product/Product.vue
+++ src/components/Product/Product.vue
@@ -19,7 +19,7 @@ const updateWindowWidth = () => {
   // 視窗 910px
   windowWidth.value = window.innerWidth;
   // hideAddImgProduct
-  hideAddImgProduct.value = window.matchMedia("(max-width: 600px)").matches;
+  // hideAddImgProduct.value = window.matchMedia("(max-width: 600px)").matches;
 };
 
 const loading = ref(true);
