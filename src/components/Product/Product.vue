e23f98bb47d8c8b69de2fa9c9b4fedf60685ed47
DaLan <valkyrie777is@gmail.com>
2025-01-26 09:56:10 +0800

    修改96

--- src/components/Product/Product.vue
+++ src/components/Product/Product.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted, onBeforeUnmount } from "vue";
+import { ref, onMounted, onBeforeUnmount, computed } from "vue";
 import { storeToRefs } from "pinia";
 import { useProductStore } from "@/stores/products";
 import { useMenuStore } from "@/stores/menu";
