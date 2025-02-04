387af64da76c6f313a463c3cd24725d9392b4eb1
DaLan <valkyrie777is@gmail.com>
2025-02-04 10:15:19 +0800

    修改37

--- src/components/AppLayout/Menu.vue
+++ src/components/AppLayout/Menu.vue
@@ -125,7 +125,7 @@ onUnmounted(() => {
   position: relative;
   flex-direction: column;
   min-height: 100vh;
-  width: 200px;
+  width: 100px;
   left: 0;
   top: 0;
   border-right: 1px solid #d0d0d0;
