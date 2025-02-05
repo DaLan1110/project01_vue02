c3bf9d37e44f1ca8fad48d2564f087ebed252080
DaLan <valkyrie777is@gmail.com>
2025-02-05 10:15:20 +0800

    修改62

--- src/stores/menu.js
+++ src/stores/menu.js
@@ -5,7 +5,7 @@ export const useMenuStore = defineStore('menu', () => {
     const menuFlexible = ref(false);
     // 根據 menuFlexible 和是否為覆蓋模式計算 hideAddImgProduct
     const hideAddImgProduct = computed(() => {
-        return !menuFlexible.value && !isOverlay.value;
+        return !menuFlexible.value;
     });
     const toggleMenu = () => {
         menuFlexible.value = !menuFlexible.value;
