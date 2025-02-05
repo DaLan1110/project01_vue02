cebdfb8c087b5150898b83f0ed378c40bef06fd1
DaLan <valkyrie777is@gmail.com>
2025-02-05 16:27:40 +0800

    修改07

--- src/views/AppLayout.vue
+++ src/views/AppLayout.vue
@@ -24,6 +24,9 @@ import NavLayout from "@/components/AppLayout/NavLayout.vue";
 
 .app {
   display: flex;
+  width: 100%; /* 使寬度隨螢幕大小調整 */
+  height: 100%; /* 使高度隨螢幕大小調整 */
+  overflow: auto; /* 如果內容超過容器，啟用滾動條 */
 }
 
 @media (max-width: 768px) {
