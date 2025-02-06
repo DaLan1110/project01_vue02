dbe42bf11216cba369df5aee0f638ff0235abef1
DaLan <valkyrie777is@gmail.com>
2025-02-06 15:39:36 +0800

    修改38

--- src/components/Join/Join.vue
+++ src/components/Join/Join.vue
@@ -559,7 +559,7 @@ onBeforeUnmount(() => {
   }
 }
 
-@media (max-width: 1230px) {
+@media (max-width: 1240px) {
   /* join-1-table */
   .join-1-table thead th:nth-child(4),
   .join-1-table tbody td:nth-child(4) {
