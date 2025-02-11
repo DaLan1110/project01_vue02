18ddfab5a5577f3e9ee4b104a52c6acab5a0494b
DaLan <valkyrie777is@gmail.com>
2025-02-12 07:13:41 +0800

    修改36

--- src/components/Product/ProductUpdate.vue
+++ src/components/Product/ProductUpdate.vue
@@ -184,29 +184,22 @@ const onInputBlur = (e, inputText) => {
 
 const isFormValid = computed(() => {
   // 確保 newProductData 存在並且所有屬性都有值
-  // watch(
-  //   () => [
-  //     product.value.product_name
-  //   ],
-  //   ([newSweet, newIce, newAddress, newAdd], [oldIce, oldAddress]) => {
-  //     console.log(newSweet, newIce, newAddress, newAdd);
-  //     product.value.product_sweetness = selectedSweetness.value;
-  //     product.value.product_ice = selectedIce.value;
-  //     product.value.product_address = selectedAddress.value;
-  //     product.value.product_add = selectedAdd.value;
-  //   }
-  // );
+  watch(
+    () => [product.value.product_name],
+    ([newName]) => {
+      console.log(newName);
+    }
+  );
 
   watch(
     () => [
-      product.value.product_name,
       selectedSweetness.value,
       selectedIce.value,
       selectedAddress.value,
       selectedAdd.value,
     ],
-    ([newName, newSweet, newIce, newAddress, newAdd], [oldIce, oldAddress]) => {
-      console.log(newName, newSweet, newIce, newAddress, newAdd);
+    ([newSweet, newIce, newAddress, newAdd], [oldIce, oldAddress]) => {
+      console.log(newSweet, newIce, newAddress, newAdd);
       product.value.product_sweetness = selectedSweetness.value;
       product.value.product_ice = selectedIce.value;
       product.value.product_address = selectedAddress.value;
