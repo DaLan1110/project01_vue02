32bb1e0c532067620fd5f34312079ced46e2cf04
DaLan <valkyrie777is@gmail.com>
2025-02-12 07:00:42 +0800

    修改32

--- src/components/Product/ProductUpdate.vue
+++ src/components/Product/ProductUpdate.vue
@@ -186,7 +186,6 @@ const isFormValid = computed(() => {
   // 確保 newProductData 存在並且所有屬性都有值
   watch(
     () => [
-      product.value.product_name,
       selectedSweetness.value,
       selectedIce.value,
       selectedAddress.value,
