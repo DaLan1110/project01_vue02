99b3cbb87b2c3d665a7e9fe12e48d045e270cf2c
DaLan <valkyrie777is@gmail.com>
2025-02-12 07:24:52 +0800

    修改37

--- src/components/Product/ProductUpdate.vue
+++ src/components/Product/ProductUpdate.vue
@@ -182,31 +182,54 @@ const onInputBlur = (e, inputText) => {
     : (errors.value[inputText] = null);
 };
 
+watch(
+  () => [
+    selectedSweetness.value,
+    selectedIce.value,
+    selectedAddress.value,
+    selectedAdd.value,
+  ],
+  ([newSweet, newIce, newAddress, newAdd], [oldIce, oldAddress]) => {
+    console.log(newSweet, newIce, newAddress, newAdd);
+    product.value.product_sweetness = selectedSweetness.value;
+    product.value.product_ice = selectedIce.value;
+    product.value.product_address = selectedAddress.value;
+    product.value.product_add = selectedAdd.value;
+  }
+);
+
+watch(
+  () => [product.value.product_name],
+  ([newName]) => {
+    console.log(newName);
+  }
+);
+
 const isFormValid = computed(() => {
   // 確保 newProductData 存在並且所有屬性都有值
-  watch(
-    () => [product.value.product_name],
-    ([newName]) => {
-      console.log(newName);
-      product.value.product_name = product.value.product_name;
-    }
-  );
+  // watch(
+  //   () => [product.value.product_name],
+  //   ([newName]) => {
+  //     console.log(newName);
+  //     product.value.product_name = product.value.product_name;
+  //   }
+  // );
 
-  watch(
-    () => [
-      selectedSweetness.value,
-      selectedIce.value,
-      selectedAddress.value,
-      selectedAdd.value,
-    ],
-    ([newSweet, newIce, newAddress, newAdd], [oldIce, oldAddress]) => {
-      console.log(newSweet, newIce, newAddress, newAdd);
-      product.value.product_sweetness = selectedSweetness.value;
-      product.value.product_ice = selectedIce.value;
-      product.value.product_address = selectedAddress.value;
-      product.value.product_add = selectedAdd.value;
-    }
-  );
+  // watch(
+  //   () => [
+  //     selectedSweetness.value,
+  //     selectedIce.value,
+  //     selectedAddress.value,
+  //     selectedAdd.value,
+  //   ],
+  //   ([newSweet, newIce, newAddress, newAdd], [oldIce, oldAddress]) => {
+  //     console.log(newSweet, newIce, newAddress, newAdd);
+  //     product.value.product_sweetness = selectedSweetness.value;
+  //     product.value.product_ice = selectedIce.value;
+  //     product.value.product_address = selectedAddress.value;
+  //     product.value.product_add = selectedAdd.value;
+  //   }
+  // );
 
   if (
     !product.value ||
