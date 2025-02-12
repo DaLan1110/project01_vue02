8a6c3226be7fdda8451a2a695bf9d636c6f1f06f
DaLan <valkyrie777is@gmail.com>
2025-02-12 09:23:12 +0800

    修改53

--- src/components/Product/ProductUpdate.vue
+++ src/components/Product/ProductUpdate.vue
@@ -184,9 +184,9 @@ const onInputBlur = (e, inputText) => {
 };
 
 // 確保 newProductData 存在並且所有屬性都有值
-// watch(product, (newVal) => {
-//   console.log("product 變更:", newVal);
-// });
+watch(product, (newVal) => {
+  console.log("product 變更:", newVal);
+});
 
 watch(
   () => [
