abe84d409da31e2266b4b2f90e1efd75e226e18d
DaLan <valkyrie777is@gmail.com>
2025-02-15 06:56:25 +0800

    修改01

--- src/components/Img/AddImg_productData.vue
+++ src/components/Img/AddImg_productData.vue
@@ -38,7 +38,7 @@ watch(
 
 <template>
   <div class="row align-items-center productData-responsive-justify">
-    <div class="col-10" style="margin-top: 65px">
+    <div class="col-10">
       <label for="inputGroupFile01">產品圖片</label>
       <input
         type="file"
