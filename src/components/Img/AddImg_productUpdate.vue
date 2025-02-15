126bf105b9b6263306f397be519a64ea329bbace
DaLan <valkyrie777is@gmail.com>
2025-02-15 13:18:49 +0800

    修改34

--- src/components/Img/AddImg_productUpdate.vue
+++ src/components/Img/AddImg_productUpdate.vue
@@ -45,7 +45,7 @@ const handleFileChange = (event) => {
         @change="handleFileChange"
       />
     </div>
-    <div class="col-7 d-flex justify-content-center mt-4">
+    <div class="col-10 d-flex justify-content-center mt-4">
       <div class="product-style-img">
         <i
           class="material-icons icon-style-avatar"
