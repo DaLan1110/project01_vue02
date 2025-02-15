63af137d100febe126c1de0995af94b431c944a9
DaLan <valkyrie777is@gmail.com>
2025-02-15 10:08:20 +0800

    修改11

--- src/components/AppLayout/Menu.vue
+++ src/components/AppLayout/Menu.vue
@@ -142,7 +142,7 @@ onUnmounted(() => {
 .burger-menu-style {
   position: fixed !important;
   z-index: 990;
-  left: calc(100% + 20px); /* 距離容器右側保持固定距離 */
+  left: calc(210px); /* 距離容器右側保持固定距離 */
   top: 20px;
   font-size: 20px;
   user-select: none;
