96318e309cf47361cc7a7a2fd908da62c9054a62
DaLan <valkyrie777is@gmail.com>
2025-02-16 09:58:31 +0800

    修改05

--- src/hook/validationHook.js
+++ src/hook/validationHook.js
@@ -1,7 +1,7 @@
 import { ref, watch } from 'vue';
 
 // const EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{3,}))$/;
-const EMAIL_REGEX = /^[a-zA-Z0-9]+([._%+-][a-zA-Z0-9]+)*@[a-zA-Z0-9.-]+\.[cC][oO][mM]$/;
+const EMAIL_REGEX = /^[a-zA-Z0-9]+([._%+-][a-zA-Z0-9]+)*@[a-zA-Z0-9.-]+\.com$/;
 const LOWER_CASE_REGEX = /(.*[a-z].*)/;
 const UPPER_CASE_REGEX = /(.*[A-Z].*)/;
 const DIGITS_REGEX = /(.*\d.*)/;
